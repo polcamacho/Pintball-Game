c64cd4a3c03df82e420568337f5417ef881685ef
polcamacho <47743832+polcamacho@users.noreply.github.com>
2019-10-31 19:45:16 +0100

    previous commit readjusted

--- Physics2_class4 - handout/ModulePhysics.cpp
+++ Physics2_class4 - handout/ModulePhysics.cpp
@@ -111,32 +111,32 @@ PhysBody* ModulePhysics::CreateRectangle(int x, int y, int width, int height)
 	return pbody;
 }
 
-//PhysBody* ModulePhysics::CreateRectangleSensor(int x, int y, int width, int height)
-//{
-//	b2BodyDef body;
-//	body.type = b2_staticBody;
-//	body.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
-//
-//	b2Body* b = world->CreateBody(&body);
-//
-//	b2PolygonShape box;
-//	box.SetAsBox(PIXEL_TO_METERS(width) * 0.5f, PIXEL_TO_METERS(height) * 0.5f);
-//
-//	b2FixtureDef fixture;
-//	fixture.shape = &box;
-//	fixture.density = 1.0f;
-//	fixture.isSensor = true;
-//
-//	b->CreateFixture(&fixture);
-//
-//	PhysBody* pbody = new PhysBody();
-//	pbody->body = b;
-//	b->SetUserData(pbody);
-//	pbody->width = width;
-//	pbody->height = height;
-//
-//	return pbody;
-//}
+PhysBody* ModulePhysics::CreateRectangleSensor(int x, int y, int width, int height)
+{
+	b2BodyDef body;
+	body.type = b2_staticBody;
+	body.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* b = world->CreateBody(&body);
+
+	b2PolygonShape box;
+	box.SetAsBox(PIXEL_TO_METERS(width) * 0.5f, PIXEL_TO_METERS(height) * 0.5f);
+
+	b2FixtureDef fixture;
+	fixture.shape = &box;
+	fixture.density = 1.0f;
+	fixture.isSensor = true;
+
+	//b->CreateFixture(&fixture);
+
+	PhysBody* pbody = new PhysBody();
+	pbody->body = b;
+	b->SetUserData(pbody);
+	pbody->width = width;
+	pbody->height = height;
+
+	return pbody;
+}
 
 PhysBody* ModulePhysics::CreateChain(int x, int y, int* points, int size)
 {
